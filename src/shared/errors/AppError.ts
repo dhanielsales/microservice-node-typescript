007e493e14e5064e3d38b0718447fbc3d14b65cd
Dhanielr <dhanielr@hotmail.com>
2021-08-28 21:25:36 -0300

    Ajustes no AppError

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,9 +1,11 @@
 export default class AppError {
   public readonly message: string;
+  public readonly details: any;
   public readonly statusCode: number;
 
-  constructor(message: string, statusCode = 400) {
+  constructor(message: string, statusCode = 400, details?: any) {
     this.message = message;
+    this.details = details;
     this.statusCode = statusCode;
   }
 }
